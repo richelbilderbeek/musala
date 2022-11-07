6c47c97b2f9c5667261770d2428c63c11286710b
richelbilderbeek <richel@richelbilderbeek.nl>
2022-11-07 19:53:58 +0100

    Simplified code

--- README.md
+++ README.md
@@ -12,6 +12,41 @@ Arduino 3.3 V         |PIR Vin
 Arduino GND           |PIR GND
 Arduino 12            |PIR out
 
+
+### Troubleshooting
+
+Port not found
+
+Earlier reported at [here](https://forum.arduino.cc/t/arduino-ide-2-0-not-showing-ports/858267/13)
+
+Ideed:
+
+```
+sudo dmesg -w
+```
+
+Results in:
+
+
+```
+[ 3352.611354] usb 1-4: new full-speed USB device number 12 using xhci_hcd
+[ 3352.761926] usb 1-4: New USB device found, idVendor=1a86, idProduct=7523, bcdDevice= 2.64
+[ 3352.761939] usb 1-4: New USB device strings: Mfr=0, Product=2, SerialNumber=0
+[ 3352.761944] usb 1-4: Product: USB Serial
+[ 3352.764411] ch341 1-4:1.0: ch341-uart converter detected
+[ 3352.765467] usb 1-4: ch341-uart converter now attached to ttyUSB0
+[ 3353.314104] input: BRLTTY 6.4 Linux Screen Driver Keyboard as /devices/virtual/input/input32
+[ 3353.436461] usb 1-4: usbfs: interface 0 claimed by ch341 while 'brltty' sets config #1
+[ 3353.437038] ch341-uart ttyUSB0: ch341-uart converter now disconnected from ttyUSB0
+[ 3353.437051] ch341 1-4:1.0: device disconnected
+```
+
+Solution: uninstall `brltty` by doing:
+
+```
+sudo apt-get remove brltty
+```
+
 ## Links
 
  * [SparkFun board for Arduino IDE](https://github.com/sparkfun/Arduino_Boards.git)
@@ -20,4 +55,5 @@ Arduino 12            |PIR out
  * [CH340 driver download page](http://www.wch-ic.com/downloads/CH341SER_ZIP.html)
    download at `http://www.wch-ic.com/downloads/CH341SER_LINUX_ZIP.html`
  * [Old CH340 driver download page](https://learn.sparkfun.com/tutorials/how-to-install-ch340-drivers)
+
 
--- musala.ino
+++ musala.ino
@@ -1,116 +1,40 @@
-
-/*
-  Controlling the Qwiic MP3 Trigger with I2C Commands
-  By: Nathan Seidle
-  SparkFun Electronics
-  Date: January 12th, 2019
-  License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
-
-  This example plays the first track on the SD card.
-
-  Feel like supporting open source hardware?
-  Buy a board from SparkFun! https://www.sparkfun.com/products/15165
-
-  Hardware Connections:
-  Plug in headphones
-  Make sure the SD card is in the socket and has some MP3s in the root directory
-  Don't have a USB cable connected to the Qwiic MP3 Trigger right now
-  If needed, attach a Qwiic Shield to your Arduino/Photon/ESP32 or other
-  Plug the Qwiic device onto an available Qwiic port
-  Open the serial monitor at 115200 baud
-*/
-
 #include <Wire.h> //Needed for I2C to Qwiic MP3 Trigger
-
 #include "SparkFun_Qwiic_MP3_Trigger_Arduino_Library.h" //http://librarymanager/All#SparkFun_MP3_Trigger
+
 MP3TRIGGER mp3;
-
-const int PIRmotion = 12; // choose the input pin (for PIR sensor)
-int PIRstate = LOW; // we start, assuming no motion detected
-int PIRval = 0; // variable for reading the pin status
-
-boolean is_playing_music = false;
-int time_playing_music_sec = 0;
+const int pir_pin = 12; // choose the input pin (for PIR sensor)
 const int music_length_sec = 241;
+const int music_length_msec = 1000 * music_length_sec;
 
 void setup()
 {
-  Serial.println("Setup");
-  pinMode(PIRmotion, INPUT); // declare sensor as input
-  pinMode(13, OUTPUT); // LED on breadboard
-  pinMode(3, OUTPUT); // Trigger of MP3 player
-
-  // Turn on pin for 100 msec at startup to say hi
+  pinMode(pir_pin, INPUT); // declare sensor as input
+  pinMode(13, OUTPUT); // Trigger of built-in LED
   digitalWrite(13, HIGH);
-  delay(100);
-  digitalWrite(13, LOW);
-  delay(100);
-
-
-  Serial.println("Setting up software serial");
   Serial.begin(115200);
-
   Wire.begin();
-  delay(10000);
- 
 
-  //Check to see if Qwiic MP3 is present on the bus
-  if (mp3.begin() == false)
+  if (!mp3.begin())
   {
     Serial.println("Qwiic MP3 failed to respond. Please check wiring and possibly the I2C address. Freezing...");
-    while (1)
-      ;
+    while (1) { ; }
   }
+  mp3.setVolume(0); //Volume can be 0 (off) to 31 (max)
 
-
-  Serial.println("Give MP3 card time to start, 2 sec");
-  delay(2000);
-  mp3.setVolume(25); //Volume can be 0 (off) to 31 (max)
-  mp3.stop();
-  //mp3.clearInterrupts();
-  digitalWrite(3, HIGH); // ?????'When trigger pin is pulled to GND, the file is played'
-
-  // Turn on pin for 200 msec at startup to say setup is done
-  //digitalWrite(13, HIGH);
-  //delay(200);
-  //digitalWrite(13, LOW);
-  //delay(200);
-
-  Serial.println("Setup is done");
+  // Start MP3 card
+  delay(20000);
 }
 
 void loop()
 {
-  if (is_playing_music)
+  digitalWrite(13, LOW);
+  if (digitalRead(pir_pin) == HIGH)
   {
-    ++time_playing_music_sec;
-    Serial.print("Jag spelar musik nu den ");
-    Serial.print(time_playing_music_sec);
-    Serial.println(":e second");
-    digitalWrite(3, LOW); // Trigger music
     digitalWrite(13, HIGH);
-  }
-  else
-  {
-    const int PIRval = digitalRead(PIRmotion); // read input value
-    if (PIRval == HIGH)
-    {
-      Serial.println("Jag startar musik nu ...");
-      is_playing_music = true;
-      time_playing_music_sec = 0;
-      mp3.playTrack(1);
-    }
-    else
-    {
-      digitalWrite(3, HIGH); // Do not trigger music
-    }
-    digitalWrite(13, LOW);
-  }
-  if (time_playing_music_sec > music_length_sec)
-  {
-    Serial.println("Jag stoppar musik nu ...");
-    is_playing_music = false;
+    mp3.setVolume(1);
+    mp3.playTrack(1);
+    delay(music_length_msec);
     mp3.stop();
   }
-  delay(1000);
+  delay(100);
 }
